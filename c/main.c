c99be34b2192506d4b9855e8e74ba544b9f0e53c
JadKHaddad <jadkhaddad@gmail.com>
2022-11-04 00:57:13 +0100

    C

--- c/main.c
+++ c/main.c
@@ -1,38 +1,35 @@
 #include <time.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <stdio.h>
 /* for ETIMEDOUT */
 #include <errno.h>
 #include <string.h>
 
-pthread_mutex_t calculating = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t done = PTHREAD_COND_INITIALIZER;
-
-void *expensive_call(void *data)
+typedef struct
 {
-        int oldtype;
+        int bar;
+        int baz;
+} foo;
 
-        /* allow the thread to be killed at any time */
-        pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &oldtype);
-
-        /* ... calculations and expensive io here, for example:
-         * infinitely loop
-         */
-        for (;;) {}
-
-        /* wake up the caller if we've completed in time */
-        pthread_cond_signal(&done);
-        return NULL;
-}
-
-/* note: this is not thread safe as it uses a global condition/mutex */
-int do_or_timeout(struct timespec *max_wait)
+int run_with_timeout(void (*f)(void *in, void *out, int *err), void *in, void *out, int *err, struct timespec *max_wait)
 {
+        pthread_mutex_t calculating = PTHREAD_MUTEX_INITIALIZER;
+        pthread_cond_t done = PTHREAD_COND_INITIALIZER;
+
         struct timespec abs_time;
         pthread_t tid;
-        int err;
 
-        pthread_mutex_lock(&calculating);
+        void *expensive_call(void *data)
+        {
+                /* ... calculations and expensive io here */
+                f(in, out, err);
+
+                /* wake up the caller if we've completed in time */
+                pthread_cond_signal(&done);
+
+                return NULL;
+        }
 
         /* pthread cond_timedwait expects an absolute time to wait until */
         clock_gettime(CLOCK_REALTIME, &abs_time);
@@ -44,26 +41,109 @@ int do_or_timeout(struct timespec *max_wait)
         /* pthread_cond_timedwait can return spuriously: this should
          * be in a loop for production code
          */
-        err = pthread_cond_timedwait(&done, &calculating, &abs_time);
+        return pthread_cond_timedwait(&done, &calculating, &abs_time);
+}
 
-        if (err == ETIMEDOUT)
-                fprintf(stderr, "%s: calculation timed out\n", __func__);
+void infinite_loop(void *in, void *out, int *err)
+{
+        while (1)
+        {
+                printf("infinite loop\n");
+                sleep(1);
+        }
+}
 
-        if (!err)
-                pthread_mutex_unlock(&calculating);
+int add(int a, int b)
+{
+        return a + b;
+}
 
-        return err;
+void scc(void *in, void *out, int *err)
+{
+        int *a = (int *)in;
+        int *b = (int *)out;
+        *b = add(*a, 1);
+}
+
+void fail(void *in, void *out, int *err)
+{
+        for (;;)
+                ;
+        int *a = (int *)in;
+        int *b = (int *)out;
+        *b = add(*a, 1);
+}
+
+void err(void *in, void *out, int *err)
+{
+        /* Lol */
+        *err = -1;
+}
+
+void give_foo(void *in, void *out, int *err)
+{
+        foo *f = (foo *)out;
+        f->bar = 1;
+        f->baz = 2;
 }
 
 int main()
 {
         struct timespec max_wait;
-
         memset(&max_wait, 0, sizeof(max_wait));
-
-        /* wait at most 2 seconds */
         max_wait.tv_sec = 2;
-        do_or_timeout(&max_wait);
 
+        int in = 1;
+        int out = 0;
+        if (run_with_timeout(scc, &in, &out, NULL, &max_wait) == ETIMEDOUT)
+        {
+                printf("timeout\n");
+        }
+        else
+        {
+                printf("res = %d\n", out);
+        }
+
+        in = 1;
+        out = 0;
+        if (run_with_timeout(fail, &in, &out, NULL, &max_wait) == ETIMEDOUT)
+        {
+                printf("timeout\n");
+        }
+        else
+        {
+                printf("res = %d\n", out);
+        }
+
+        foo f;
+        if (run_with_timeout(give_foo, NULL, &f, NULL, &max_wait) == ETIMEDOUT)
+        {
+                printf("timeout\n");
+        }
+        else
+        {
+                printf("res = foo.bar [%d] | foo.baz [%d]\n", f.bar, f.baz);
+        }
+
+        int e = 0;
+        if (run_with_timeout(err, NULL, NULL, &e, &max_wait) == ETIMEDOUT)
+        {
+                printf("timeout\n");
+        }
+        else
+        {
+                if (e == -1)
+                {
+                        printf("error\n");
+                }
+        }
+
+        /* run_with_timeout will not terminate the task*/
+        if (run_with_timeout(infinite_loop, NULL, NULL, NULL, &max_wait) == ETIMEDOUT)
+        {
+                printf("timeout\n");
+        }
+
+        sleep(10);
         return 0;
 }
